27e163e36722e216b579906efcc7d3549977cd13
Irina <afanirina@yandex.ru>
2025-10-05 22:38:29 +0300

    First task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let x1;
+  let x2;
+  let d = b ** 2 - 4 * a * c;
+
+  if (d > 0) {
+    x1 = (-b + Math.sqrt(d) ) / (2 * a);
+    x2 = (-b - Math.sqrt(d) ) / (2 * a);
+    arr.push(x1, x2);
+  } else if (d === 0) {
+    x1 = -b / (2 * a);
+    arr.push(x1);
+  }
+
   return arr;
 }
+
+console.log(solveEquation);
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
